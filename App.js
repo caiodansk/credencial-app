bfa45e1ffcb5178f2fadb228369b860a31d6d77b
caiodansk <caiovilante123@gmail.com>
2025-06-03 19:39:03 -0300

    chore(revert): removendo splash com erro no deploy

--- App.js
+++ App.js
@@ -1,36 +1,6 @@
-import React, { useCallback, useEffect, useState } from "react";
-import * as SplashScreen from 'expo-splash-screen';
+import React from "react";
 import Routes from './src/routes/Navigation';
 
-SplashScreen.preventAutoHideAsync();
-
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Simula carregamento de recursos se necessário
-        await new Promise(resolve => setTimeout(resolve, 500));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
-  return <Routes onLayout={onLayoutRootView} />;
+  return <Routes />;
 }
